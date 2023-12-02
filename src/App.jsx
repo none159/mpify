1f377c1ef99c7ddb66f418ecc1ff7bab2d8129e5
none159 <noneenone019@gmail.com>
2023-12-02 16:09:33 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import './App.css'
 import MHero from './components/MHero'
 import Footer from './components/Footer'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home"
 import Contact from './components/Contact'
 import Faq from './components/Faq'
